85cb68ad6430e14346ca1be48237afc69c1b3c18
JonathanBlackDoctor <alpaomegastartend@gmail.com>
2026-09-18 18:15:20 +0900

    Finalize sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'weight-watch-v1';
+const CACHE = 'weight-watch-v2';
 const SHELL = ['./', './index.html', './style.css', './app.js', './timer.js', './icon.svg', './manifest.webmanifest'];
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(SHELL)));
